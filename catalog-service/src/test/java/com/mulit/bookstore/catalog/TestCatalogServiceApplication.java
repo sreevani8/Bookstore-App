8bf025ee3d6dc4224163104499d34977241a5367
sreevani8 <sreevanidumpala@gmail.com>
2026-02-09 12:03:52 +0530

    Catalog Service module setup

---
+++ catalog-service/src/test/java/com/mulit/bookstore/catalog/TestCatalogServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mulit.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
