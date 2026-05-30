40c936471b72d27b19d7e430fb73479b7869ea65
sreevani8 <sreevanidumpala@gmail.com>
2026-05-30 10:05:10 +0530

    Application Properties "locahost" to "orders-db" and "catalog-db" changed where needed..

--- order-service/src/main/java/com/mulit/bookstore/orders/OrderServiceApplication.java
+++ order-service/src/main/java/com/mulit/bookstore/orders/OrderServiceApplication.java
@@ -2,12 +2,12 @@ package com.mulit.bookstore.orders;
 
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = "com.mulit")
 @EnableConfigurationProperties(ApplicationProperties.class)
 @ConfigurationPropertiesScan
 @EnableScheduling
