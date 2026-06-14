53a8e2e402297785d6316c06840e6efc1d87284b
sreevani8 <sreevanidumpala@gmail.com>
2026-06-14 12:45:43 +0530

    Added Keycloak(OAUTH2)

--- order-service/src/test/java/com/mulit/bookstore/orders/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/mulit/bookstore/orders/OrderServiceApplicationTests.java
@@ -5,7 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.testcontainers.enabled=false"
+})
 class OrderServiceApplicationTests extends AbstractIT {
 
     @Test
